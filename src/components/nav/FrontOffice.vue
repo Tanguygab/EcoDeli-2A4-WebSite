a655951d4c38306db2cc08f656904c36522cc64b
Tanguygab <tanguy@gabril.com>
2025-05-10 19:14:17 +0200

    Made user menu icon clickable to stay on screen

--- src/components/nav/FrontOffice.vue
+++ src/components/nav/FrontOffice.vue
@@ -2,9 +2,23 @@
 import { startSession } from "@/stores/session"
 import { api, logout } from "@/api";
 import Logo from "../Logo.vue";
+import { ref } from "vue";
 
 const session = startSession()
 api(session)
+
+const menuIcon = ref()
+const menu = ref(false)
+
+function openMenu() {
+    menu.value = true
+}
+
+document.addEventListener("click", e => {
+    if (e.target !== menuIcon.value) {
+        menu.value = false
+    }
+})
 
 </script>
 
@@ -15,16 +29,17 @@ api(session)
         </div>
         <div class="navbar-menu navbar-end navbar-item buttons">
             <div v-if="session.user">
-                <div class="navbar-item has-dropdown is-hoverable">
-                    <img src="@/assets/pedro.png">
+                <div :class="'navbar-item has-dropdown is-hoverable' + (menu ? ' is-active' : '')" @click="openMenu">
+                    <img ref="menuIcon" src="@/assets/pedro.png">
                     <div class="navbar-dropdown is-boxed is-right">
                         <RouterLink class="navbar-item" to="/profile">{{ $t("user.profile") }}</RouterLink>
                         <RouterLink class="navbar-item" to="/notifications">{{ $t("user.notifications") }}</RouterLink>
                         <RouterLink class="navbar-item" to="/settings">{{ $t("user.settings") }}</RouterLink>
                         <hr class="navbar-divider">
                         <RouterLink v-if="session.user.role.access_level >= 100" class="navbar-item has-text-danger-50"
-                            to="/backoffice">{{
-                                $t("user.backoffice") }}</RouterLink>
+                            to="/backoffice">
+                            {{ $t("user.backoffice") }}
+                        </RouterLink>
                         <a @click="logout()" class="navbar-item">{{ $t("auth.logout") }}</a>
                     </div>
                 </div>
