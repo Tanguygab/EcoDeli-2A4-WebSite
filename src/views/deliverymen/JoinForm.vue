131d8c9d5e268b283a06a1bf0a8bfe3bc0cdd81b
Suussonic <150890672+Suussonic@users.noreply.github.com>
2025-07-15 16:26:47 +0200

    Gestion des langage et remerciment dans delivry

--- src/views/deliverymen/JoinForm.vue
+++ src/views/deliverymen/JoinForm.vue
@@ -1,25 +1,29 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { joinDeliveries } from '@/api.ts'
+import { joinDeliveries } from '@/api'
 import router from '@/router'
 
 const form = ref<HTMLFormElement>()
+const sent = ref(false)
 
-function submit() {
-    const formData = new FormData(form.value);
-    joinDeliveries(formData).then(() => {
-        router.push('/deliverymen/')
-    })
+async function submit() {
+    const formData = new FormData(form.value)
+    await joinDeliveries(formData)
+    sent.value = true
+    // Optionnel : router.push('/deliverymen/')
 }
 </script>
 
 <template>
-  <form ref="form" class="box" @submit.prevent="submit" enctype="multipart/form-data">
-    <h2 class="title is-4 mb-4">Demande de recrutement livreur</h2>
+  <form v-if="!sent" ref="form" class="box" @submit.prevent="submit" enctype="multipart/form-data">
+    <h2 class="title is-4 mb-4">{{ $t("delivery.request-title") || "Demande de recrutement livreur" }}</h2>
     <div class="field">
-      <label class="label">Pièce justificative</label>
+      <label class="label">{{ $t("form.file") || "Pièce justificative" }}</label>
       <input class="input" name="proof" type="file" />
     </div>
-    <input type="submit" class="button is-success mt-3" value="Envoyer la demande">
+    <input type="submit" class="button is-success mt-3" :value="$t('contact.send') || 'Envoyer la demande'">
   </form>
+  <div v-else class="notification is-success has-text-centered">
+    {{ $t("delivery.thanks") || "Merci pour votre candidature ! Nous reviendrons vers vous rapidement." }}
+  </div>
 </template>
