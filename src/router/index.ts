15e3d120db4e82c5c13e6e57cf09018e92661eab
Tmelchio <Tmelchio@users.noreply.github.com>
2025-07-15 02:58:02 +0200

    { path: '/', name: 'home', component: Home }index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ import Profile from '@/views/Profile.vue'
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes: [
-        { path: '/', component: Home },
+        { path: '/', name: 'home', component: Home }
         { path: '/login', component: () => import('@/views/auth/Login.vue') },
         { path: '/register', component: () => import('@/views/auth/Register.vue') },
         { path: '/profile', component: Profile },
